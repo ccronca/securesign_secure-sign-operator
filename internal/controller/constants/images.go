b5157f0933ef68333cf502e516a81c9a23fa6ca8
Firas Ghanmi <fghanmi@redhat.com>
2024-08-08 19:35:16 +0200

    updates(images+ctlog cm)

--- internal/controller/constants/images.go
+++ internal/controller/constants/images.go
@@ -12,14 +12,16 @@ var (
 
 	RekorRedisImage = "registry.redhat.io/rhtas/trillian-redis-rhel9@sha256:f9446fddb68b7b0f8a260e9491d824ccb6eff1f2fb83696383d90f593179726a"
 	// RekorServerImage   = "registry.redhat.io/rhtas/rekor-server-rhel9@sha256:73767ee1df9c9e63d5d20f5eee83dcb828fea4c5400ee771114577c02f335f66"
-	RekorServerImage   = "quay.io/securesign/rekor-server@sha256:885aa7f45c10bf2f6102f4e51981e54525f2600c10889ae1078c0eb0b29221fb"
+	// RekorServerImage   = "quay.io/securesign/rekor-server@sha256:885aa7f45c10bf2f6102f4e51981e54525f2600c10889ae1078c0eb0b29221fb"
+	RekorServerImage   = "registry.redhat.io/rhtas/rekor-server-rhel9@sha256:885aa7f45c10bf2f6102f4e51981e54525f2600c10889ae1078c0eb0b29221fb"
 	RekorSearchUiImage = "registry.redhat.io/rhtas/rekor-search-ui-rhel9@sha256:8486888c1af02c4d6bade9d00e2c84589b206e2e195f266c03ed1937babcdaa7"
 	BackfillRedisImage = "registry.redhat.io/rhtas/rekor-backfill-redis-rhel9@sha256:fd88470986177361fa58b5283ed807c19638c30be9f1391252f727db768543f8"
 
 	TufImage = "registry.redhat.io/rhtas/tuf-server-rhel9@sha256:3c9f0639534786f05a5b2a12ebadeb3c9c13fa66785e0e0470fe271406fe6a7c"
 
 	// CTLogImage = "registry.redhat.io/rhtas/certificate-transparency-rhel9@sha256:1588b8e97456806b4f76f8e1e63f3c089f2bc3b898f18f43c22d03dbc03b4c15"
-	CTLogImage = "quay.io/securesign/certificate-transparency-go@sha256:7b746b96e534ec8d6cee0b3b12b36c0b313a547d0335cd78f0df4b939d12c09a"
+	// CTLogImage = "quay.io/securesign/certificate-transparency-go@sha256:7b746b96e534ec8d6cee0b3b12b36c0b313a547d0335cd78f0df4b939d12c09a"
+	CTLogImage = "registry.redhat.io/rhtas/certificate-transparency-rhel9@sha256:7b746b96e534ec8d6cee0b3b12b36c0b313a547d0335cd78f0df4b939d12c09a"
 
 	ClientServerImage    = "registry.access.redhat.com/ubi9/httpd-24@sha256:7874b82335a80269dcf99e5983c2330876f5fe8bdc33dc6aa4374958a2ffaaee"
 	ClientServerImage_cg = "registry.redhat.io/rhtas/client-server-cg-rhel9@sha256:105abb4bc44fc15b62df386be45d75bea40429aee5d2cbb3d599b8614e1f7b13"
