securesign/secure-sign-operator

improve testing, apply requested changes

internal/controller/tsa/tsa_hot_update_test.go
@@ -1,3 +1,5 @@
+package tsa
+
 /*
 Copyright 2023.
 
@@ -14,51 +16,44 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package tsa
-
 import (
 	"context"
 	"time"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	"k8s.io/apimachinery/pkg/api/errors"
-	"k8s.io/apimachinery/pkg/api/meta"
-	"k8s.io/apimachinery/pkg/types"
-
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-
 	rhtasv1alpha1 "github.com/securesign/operator/api/v1alpha1"
 	"github.com/securesign/operator/internal/controller/constants"
 	"github.com/securesign/operator/internal/controller/tsa/actions"
+	common "github.com/securesign/operator/internal/testing/common/tsa"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
-	v1 "k8s.io/api/networking/v1"
+	"k8s.io/apimachinery/pkg/api/equality"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/api/meta"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 )
 
-var _ = Describe("TimestampAuthority Controller", func() {
-	Context("When reconciling a resource", func() {
+var _ = Describe("Timestamp Authority hot update", func() {
+	Context("Timestamp Authority hot update test", func() {
 
 		const (
 			Name      = "test-tsa"
-			Namespace = "default"
+			Namespace = "update"
 		)
 
 		ctx := context.Background()
 
 		namespace := &corev1.Namespace{
 			ObjectMeta: metav1.ObjectMeta{
-				Name:      Name,
-				Namespace: Namespace,
+				Name: Namespace,
 			},
 		}
 
 		typeNamespaceName := types.NamespacedName{Name: Name, Namespace: Namespace}
 		timestampAuthority := &rhtasv1alpha1.TimestampAuthority{}
 		found := &rhtasv1alpha1.TimestampAuthority{}
-		deployment := &appsv1.Deployment{}
-		service := &corev1.Service{}
-		ingress := &v1.Ingress{}
 
 		BeforeEach(func() {
 			By("Creating the Namespace to perform the tests")
@@ -131,72 +126,14 @@ var _ = Describe("TimestampAuthority Controller", func() {
 				err = k8sClient.Create(ctx, tsa)
 				Expect(err).To(Not(HaveOccurred()))
 			}
+			deployment := &appsv1.Deployment{}
 
 			By("Checking if the custom resource was successfully created")
 			Eventually(func() error {
 				return k8sClient.Get(ctx, typeNamespaceName, found)
 			}).Should(Succeed())
 
-			By("Status conditions are initialized")
-			Eventually(func(g Gomega) bool {
-				g.Expect(k8sClient.Get(ctx, typeNamespaceName, found)).Should(Succeed())
-				return meta.IsStatusConditionPresentAndEqual(found.Status.Conditions, constants.Ready, metav1.ConditionFalse)
-			}).Should(BeTrue())
-
-			By("Tsa signer should be resolved")
-			Eventually(func(g Gomega) bool {
-				g.Expect(k8sClient.Get(ctx, typeNamespaceName, found)).Should(Succeed())
-				return meta.IsStatusConditionPresentAndEqual(found.Status.Conditions, actions.TSASignerCondition, metav1.ConditionTrue)
-			}).Should(BeTrue())
-
-			By("Certificate chain secret should be created")
-			Eventually(func(g Gomega) *rhtasv1alpha1.SecretKeySelector {
-				g.Expect(k8sClient.Get(ctx, typeNamespaceName, found)).Should(Succeed())
-				return found.Status.Signer.CertificateChain.CertificateChainRef
-			}).Should(Not(BeNil()))
-			Expect(k8sClient.Get(ctx, types.NamespacedName{Name: found.Status.Signer.CertificateChain.CertificateChainRef.Name, Namespace: Namespace}, &corev1.Secret{})).Should(Succeed())
-
-			By("File Signer secret should be created")
-			Eventually(func(g Gomega) *rhtasv1alpha1.SecretKeySelector {
-				g.Expect(k8sClient.Get(ctx, typeNamespaceName, found)).Should(Succeed())
-				return found.Status.Signer.File.PrivateKeyRef
-			}).Should(Not(BeNil()))
-			Expect(k8sClient.Get(ctx, types.NamespacedName{Name: found.Status.Signer.File.PrivateKeyRef.Name, Namespace: Namespace}, &corev1.Secret{})).Should(Succeed())
-
-			By("Should be in a creating phase")
-			Eventually(func(g Gomega) string {
-				g.Expect(k8sClient.Get(ctx, typeNamespaceName, found)).Should(Succeed())
-				return meta.FindStatusCondition(found.Status.Conditions, constants.Ready).Reason
-			}).Should(Equal(constants.Creating))
-
-			By("NTP monitoring should be resolved")
-			Eventually(func(g Gomega) string {
-				g.Expect(k8sClient.Get(ctx, typeNamespaceName, found)).Should(Succeed())
-				return meta.FindStatusCondition(found.Status.Conditions, constants.Ready).Message
-			}).Should(Equal("NTP monitoring configured"))
-
-			By("NTP monitoring config should be created")
-			Eventually(func(g Gomega) *rhtasv1alpha1.LocalObjectReference {
-				g.Expect(k8sClient.Get(ctx, typeNamespaceName, found)).Should(Succeed())
-				return found.Status.NTPMonitoring.Config.NtpConfigRef
-			}).Should(Not(BeNil()))
-			Expect(k8sClient.Get(ctx, types.NamespacedName{Name: found.Status.NTPMonitoring.Config.NtpConfigRef.Name, Namespace: Namespace}, &corev1.ConfigMap{})).Should(Succeed())
-
-			By("Timestamp Authority service is created")
-			Eventually(func() error {
-				return k8sClient.Get(ctx, types.NamespacedName{Name: actions.DeploymentName, Namespace: Namespace}, service)
-			}).Should(Succeed())
-			Expect(service.Spec.Ports[0].Port).Should(Equal(int32(3000)))
-
-			By("Checking if Ingress was successfully created in the reconciliation")
-			Eventually(func() error {
-				return k8sClient.Get(ctx, types.NamespacedName{Name: actions.DeploymentName, Namespace: Namespace}, ingress)
-			}).Should(Succeed())
-			Expect(ingress.Spec.Rules[0].Host).Should(Equal("tsa.localhost"))
-			Expect(ingress.Spec.Rules[0].IngressRuleValue.HTTP.Paths[0].Backend.Service.Name).Should(Equal(service.Name))
-			Expect(ingress.Spec.Rules[0].IngressRuleValue.HTTP.Paths[0].Backend.Service.Port.Name).Should(Equal(actions.ServerPortName))
-
-			By("Timestamp Authority deployment is created")
+			By("Checking if Deployment was successfully created in the reconciliation")
 			Eventually(func() error {
 				return k8sClient.Get(ctx, types.NamespacedName{Name: actions.DeploymentName, Namespace: Namespace}, deployment)
 			}).Should(Succeed())
@@ -207,11 +144,103 @@ var _ = Describe("TimestampAuthority Controller", func() {
 				{Status: corev1.ConditionTrue, Type: appsv1.DeploymentAvailable, Reason: constants.Ready}}
 			Expect(k8sClient.Status().Update(ctx, deployment)).Should(Succeed())
 
-			By("Waiting until Timestamp Authority instance is Ready")
+			By("Waiting until Timestamp Authority is Ready")
 			Eventually(func(g Gomega) bool {
 				g.Expect(k8sClient.Get(ctx, typeNamespaceName, found)).Should(Succeed())
 				return meta.IsStatusConditionTrue(found.Status.Conditions, constants.Ready)
 			}).Should(BeTrue())
+
+			By("Cert and Key rotation")
+			Eventually(func(g Gomega) error {
+				g.Expect(k8sClient.Get(ctx, typeNamespaceName, found)).Should(Succeed())
+				found.Spec.Signer.CertificateChain = rhtasv1alpha1.CertificateChain{
+					CertificateChainRef: &rhtasv1alpha1.SecretKeySelector{
+						LocalObjectReference: rhtasv1alpha1.LocalObjectReference{
+							Name: "tsa-test-secret",
+						},
+						Key: "certChain",
+					},
+				}
+
+				found.Spec.Signer.File = &rhtasv1alpha1.File{
+					PrivateKeyRef: &rhtasv1alpha1.SecretKeySelector{
+						LocalObjectReference: rhtasv1alpha1.LocalObjectReference{
+							Name: "tsa-test-secret",
+						},
+						Key: "leafKey",
+					},
+					PasswordRef: &rhtasv1alpha1.SecretKeySelector{
+						LocalObjectReference: rhtasv1alpha1.LocalObjectReference{
+							Name: "tsa-test-secret",
+						},
+						Key: "leafKeyPass",
+					},
+				}
+				return k8sClient.Update(ctx, found)
+			}).Should(Succeed())
+
+			By("Pending phase until new keys and certs are resolved")
+			Eventually(func(g Gomega) string {
+				g.Expect(k8sClient.Get(ctx, typeNamespaceName, found)).Should(Succeed())
+				return meta.FindStatusCondition(found.Status.Conditions, constants.Ready).Reason
+			}).Should(Equal(constants.Pending))
+
+			By("Creating new certificate chain and signer keys")
+			secret, err := common.HandleTsaTestSecret(found, nil)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(k8sClient.Create(context.TODO(), secret)).NotTo(HaveOccurred())
+
+			By("Status field changed for cert chain")
+			Eventually(func(g Gomega) string {
+				g.Expect(k8sClient.Get(ctx, typeNamespaceName, found)).Should(Succeed())
+				return found.Status.Signer.CertificateChain.CertificateChainRef.Name
+			}).Should(Equal("tsa-test-secret"))
+
+			By("Status field changed for signer key")
+			Eventually(func(g Gomega) string {
+				g.Expect(k8sClient.Get(ctx, typeNamespaceName, found)).Should(Succeed())
+				return found.Status.Signer.File.PasswordRef.Name
+			}).Should(Equal("tsa-test-secret"))
+
+			Eventually(func(g Gomega) bool {
+				g.Expect(k8sClient.Get(ctx, typeNamespaceName, found)).Should(Succeed())
+				return meta.IsStatusConditionTrue(found.Status.Conditions, actions.TSASignerCondition)
+			}).Should(BeTrue())
+
+			By("Timestamp Authority deployment is updated")
+			Eventually(func(g Gomega) bool {
+				updated := &appsv1.Deployment{}
+				g.Expect(k8sClient.Get(ctx, types.NamespacedName{Name: actions.DeploymentName, Namespace: Namespace}, updated)).To(Succeed())
+				return equality.Semantic.DeepDerivative(deployment.Spec.Template.Spec.Volumes, updated.Spec.Template.Spec.Volumes)
+			}).Should(BeFalse())
+
+			time.Sleep(10 * time.Second)
+
+			By("NTP Monitoring update")
+			By("NTP monitoring config should be created")
+			Eventually(func(g Gomega) *rhtasv1alpha1.LocalObjectReference {
+				g.Expect(k8sClient.Get(ctx, typeNamespaceName, found)).Should(Succeed())
+				return found.Status.NTPMonitoring.Config.NtpConfigRef
+			}).Should(Not(BeNil()))
+			Expect(k8sClient.Get(ctx, types.NamespacedName{Name: found.Status.NTPMonitoring.Config.NtpConfigRef.Name, Namespace: Namespace}, &corev1.ConfigMap{})).Should(Succeed())
+
+			By("Update NTP Config")
+			Expect(k8sClient.Get(ctx, typeNamespaceName, found)).Should(Succeed())
+			found.Spec.NTPMonitoring.Config.NumServers = 2
+			Expect(k8sClient.Update(ctx, found)).To(Succeed())
+
+			By("NTP monitoring should be resolved")
+			Eventually(func(g Gomega) string {
+				g.Expect(k8sClient.Get(ctx, typeNamespaceName, found)).Should(Succeed())
+				return meta.FindStatusCondition(found.Status.Conditions, constants.Ready).Message
+			}).Should(Equal("NTP monitoring configured"))
+
+			By("Timestamp Authority deployment is updated")
+			Eventually(func(g Gomega) bool {
+				updated := &appsv1.Deployment{}
+				g.Expect(k8sClient.Get(ctx, types.NamespacedName{Name: actions.DeploymentName, Namespace: Namespace}, updated)).To(Succeed())
+				return equality.Semantic.DeepDerivative(deployment.Spec.Template.Spec.Volumes, updated.Spec.Template.Spec.Volumes)
+			}).Should(BeFalse())
 
 		})
 	})

ccronca/securesign_secure-sign-operator

internal/controller/tsa/tsa_hot_update_test.go
@@ -1,3 +1,5 @@
+package tsa
+
 /*
 Copyright 2023.
 
@@ -14,51 +16,44 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package tsa
-
 import (
 	"context"
 	"time"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	"k8s.io/apimachinery/pkg/api/errors"
-	"k8s.io/apimachinery/pkg/api/meta"
-	"k8s.io/apimachinery/pkg/types"
-
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-
 	rhtasv1alpha1 "github.com/securesign/operator/api/v1alpha1"
 	"github.com/securesign/operator/internal/controller/constants"
 	"github.com/securesign/operator/internal/controller/tsa/actions"
+	"github.com/securesign/operator/test/e2e/support"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
-	v1 "k8s.io/api/networking/v1"
+	"k8s.io/apimachinery/pkg/api/equality"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/api/meta"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 )
 
-var _ = Describe("TimestampAuthority Controller", func() {
-	Context("When reconciling a resource", func() {
+var _ = Describe("Timestamp Authority hot update", func() {
+	Context("Timestamp Authority hot update test", func() {
 
 		const (
 			Name      = "test-tsa"
-			Namespace = "default"
+			Namespace = "update"
 		)
 
 		ctx := context.Background()
 
 		namespace := &corev1.Namespace{
 			ObjectMeta: metav1.ObjectMeta{
-				Name:      Name,
-				Namespace: Namespace,
+				Name: Namespace,
 			},
 		}
 
 		typeNamespaceName := types.NamespacedName{Name: Name, Namespace: Namespace}
 		timestampAuthority := &rhtasv1alpha1.TimestampAuthority{}
 		found := &rhtasv1alpha1.TimestampAuthority{}
-		deployment := &appsv1.Deployment{}
-		service := &corev1.Service{}
-		ingress := &v1.Ingress{}
 
 		BeforeEach(func() {
 			By("Creating the Namespace to perform the tests")
@@ -131,72 +126,14 @@ var _ = Describe("TimestampAuthority Controller", func() {
 				err = k8sClient.Create(ctx, tsa)
 				Expect(err).To(Not(HaveOccurred()))
 			}
+			deployment := &appsv1.Deployment{}
 
 			By("Checking if the custom resource was successfully created")
 			Eventually(func() error {
 				return k8sClient.Get(ctx, typeNamespaceName, found)
 			}).Should(Succeed())
 
-			By("Status conditions are initialized")
-			Eventually(func(g Gomega) bool {
-				g.Expect(k8sClient.Get(ctx, typeNamespaceName, found)).Should(Succeed())
-				return meta.IsStatusConditionPresentAndEqual(found.Status.Conditions, constants.Ready, metav1.ConditionFalse)
-			}).Should(BeTrue())
-
-			By("Tsa signer should be resolved")
-			Eventually(func(g Gomega) bool {
-				g.Expect(k8sClient.Get(ctx, typeNamespaceName, found)).Should(Succeed())
-				return meta.IsStatusConditionPresentAndEqual(found.Status.Conditions, actions.TSASignerCondition, metav1.ConditionTrue)
-			}).Should(BeTrue())
-
-			By("Certificate chain secret should be created")
-			Eventually(func(g Gomega) *rhtasv1alpha1.SecretKeySelector {
-				g.Expect(k8sClient.Get(ctx, typeNamespaceName, found)).Should(Succeed())
-				return found.Status.Signer.CertificateChain.CertificateChainRef
-			}).Should(Not(BeNil()))
-			Expect(k8sClient.Get(ctx, types.NamespacedName{Name: found.Status.Signer.CertificateChain.CertificateChainRef.Name, Namespace: Namespace}, &corev1.Secret{})).Should(Succeed())
-
-			By("File Signer secret should be created")
-			Eventually(func(g Gomega) *rhtasv1alpha1.SecretKeySelector {
-				g.Expect(k8sClient.Get(ctx, typeNamespaceName, found)).Should(Succeed())
-				return found.Status.Signer.File.PrivateKeyRef
-			}).Should(Not(BeNil()))
-			Expect(k8sClient.Get(ctx, types.NamespacedName{Name: found.Status.Signer.File.PrivateKeyRef.Name, Namespace: Namespace}, &corev1.Secret{})).Should(Succeed())
-
-			By("Should be in a creating phase")
-			Eventually(func(g Gomega) string {
-				g.Expect(k8sClient.Get(ctx, typeNamespaceName, found)).Should(Succeed())
-				return meta.FindStatusCondition(found.Status.Conditions, constants.Ready).Reason
-			}).Should(Equal(constants.Creating))
-
-			By("NTP monitoring should be resolved")
-			Eventually(func(g Gomega) string {
-				g.Expect(k8sClient.Get(ctx, typeNamespaceName, found)).Should(Succeed())
-				return meta.FindStatusCondition(found.Status.Conditions, constants.Ready).Message
-			}).Should(Equal("NTP monitoring configured"))
-
-			By("NTP monitoring config should be created")
-			Eventually(func(g Gomega) *rhtasv1alpha1.LocalObjectReference {
-				g.Expect(k8sClient.Get(ctx, typeNamespaceName, found)).Should(Succeed())
-				return found.Status.NTPMonitoring.Config.NtpConfigRef
-			}).Should(Not(BeNil()))
-			Expect(k8sClient.Get(ctx, types.NamespacedName{Name: found.Status.NTPMonitoring.Config.NtpConfigRef.Name, Namespace: Namespace}, &corev1.ConfigMap{})).Should(Succeed())
-
-			By("Timestamp Authority service is created")
-			Eventually(func() error {
-				return k8sClient.Get(ctx, types.NamespacedName{Name: actions.DeploymentName, Namespace: Namespace}, service)
-			}).Should(Succeed())
-			Expect(service.Spec.Ports[0].Port).Should(Equal(int32(3000)))
-
-			By("Checking if Ingress was successfully created in the reconciliation")
-			Eventually(func() error {
-				return k8sClient.Get(ctx, types.NamespacedName{Name: actions.DeploymentName, Namespace: Namespace}, ingress)
-			}).Should(Succeed())
-			Expect(ingress.Spec.Rules[0].Host).Should(Equal("tsa.localhost"))
-			Expect(ingress.Spec.Rules[0].IngressRuleValue.HTTP.Paths[0].Backend.Service.Name).Should(Equal(service.Name))
-			Expect(ingress.Spec.Rules[0].IngressRuleValue.HTTP.Paths[0].Backend.Service.Port.Name).Should(Equal(actions.ServerPortName))
-
-			By("Timestamp Authority deployment is created")
+			By("Checking if Deployment was successfully created in the reconciliation")
 			Eventually(func() error {
 				return k8sClient.Get(ctx, types.NamespacedName{Name: actions.DeploymentName, Namespace: Namespace}, deployment)
 			}).Should(Succeed())
@@ -207,11 +144,102 @@ var _ = Describe("TimestampAuthority Controller", func() {
 				{Status: corev1.ConditionTrue, Type: appsv1.DeploymentAvailable, Reason: constants.Ready}}
 			Expect(k8sClient.Status().Update(ctx, deployment)).Should(Succeed())
 
-			By("Waiting until Timestamp Authority instance is Ready")
+			By("Waiting until Timestamp Authority is Ready")
 			Eventually(func(g Gomega) bool {
 				g.Expect(k8sClient.Get(ctx, typeNamespaceName, found)).Should(Succeed())
 				return meta.IsStatusConditionTrue(found.Status.Conditions, constants.Ready)
 			}).Should(BeTrue())
+
+			By("Cert and Key rotation")
+			Eventually(func(g Gomega) error {
+				g.Expect(k8sClient.Get(ctx, typeNamespaceName, found)).Should(Succeed())
+				found.Spec.Signer.CertificateChain = rhtasv1alpha1.CertificateChain{
+					CertificateChainRef: &rhtasv1alpha1.SecretKeySelector{
+						LocalObjectReference: rhtasv1alpha1.LocalObjectReference{
+							Name: "tsa-test-secret",
+						},
+						Key: "certificateChain",
+					},
+				}
+
+				found.Spec.Signer.File = &rhtasv1alpha1.File{
+					PrivateKeyRef: &rhtasv1alpha1.SecretKeySelector{
+						LocalObjectReference: rhtasv1alpha1.LocalObjectReference{
+							Name: "tsa-test-secret",
+						},
+						Key: "leafPrivateKey",
+					},
+					PasswordRef: &rhtasv1alpha1.SecretKeySelector{
+						LocalObjectReference: rhtasv1alpha1.LocalObjectReference{
+							Name: "tsa-test-secret",
+						},
+						Key: "leafPrivateKeyPassword",
+					},
+				}
+				return k8sClient.Update(ctx, found)
+			}).Should(Succeed())
+
+			By("Pending phase until new keys and certs are resolved")
+			Eventually(func(g Gomega) string {
+				g.Expect(k8sClient.Get(ctx, typeNamespaceName, found)).Should(Succeed())
+				return meta.FindStatusCondition(found.Status.Conditions, constants.Ready).Reason
+			}).Should(Equal(constants.Pending))
+
+			By("Creating new certificate chain and signer keys")
+			secret := support.InitTsaSecrets(Namespace, "tsa-test-secret")
+			Expect(k8sClient.Create(context.TODO(), secret)).NotTo(HaveOccurred())
+
+			By("Status field changed for cert chain")
+			Eventually(func(g Gomega) string {
+				g.Expect(k8sClient.Get(ctx, typeNamespaceName, found)).Should(Succeed())
+				return found.Status.Signer.CertificateChain.CertificateChainRef.Name
+			}).Should(Equal("tsa-test-secret"))
+
+			By("Status field changed for signer key")
+			Eventually(func(g Gomega) string {
+				g.Expect(k8sClient.Get(ctx, typeNamespaceName, found)).Should(Succeed())
+				return found.Status.Signer.File.PasswordRef.Name
+			}).Should(Equal("tsa-test-secret"))
+
+			Eventually(func(g Gomega) bool {
+				g.Expect(k8sClient.Get(ctx, typeNamespaceName, found)).Should(Succeed())
+				return meta.IsStatusConditionTrue(found.Status.Conditions, actions.TSASignerCondition)
+			}).Should(BeTrue())
+
+			By("Timestamp Authority deployment is updated")
+			Eventually(func(g Gomega) bool {
+				updated := &appsv1.Deployment{}
+				g.Expect(k8sClient.Get(ctx, types.NamespacedName{Name: actions.DeploymentName, Namespace: Namespace}, updated)).To(Succeed())
+				return equality.Semantic.DeepDerivative(deployment.Spec.Template.Spec.Volumes, updated.Spec.Template.Spec.Volumes)
+			}).Should(BeFalse())
+
+			time.Sleep(10 * time.Second)
+
+			By("NTP Monitoring update")
+			By("NTP monitoring config should be created")
+			Eventually(func(g Gomega) *rhtasv1alpha1.LocalObjectReference {
+				g.Expect(k8sClient.Get(ctx, typeNamespaceName, found)).Should(Succeed())
+				return found.Status.NTPMonitoring.Config.NtpConfigRef
+			}).Should(Not(BeNil()))
+			Expect(k8sClient.Get(ctx, types.NamespacedName{Name: found.Status.NTPMonitoring.Config.NtpConfigRef.Name, Namespace: Namespace}, &corev1.ConfigMap{})).Should(Succeed())
+
+			By("Update NTP Config")
+			Expect(k8sClient.Get(ctx, typeNamespaceName, found)).Should(Succeed())
+			found.Spec.NTPMonitoring.Config.NumServers = 2
+			Expect(k8sClient.Update(ctx, found)).To(Succeed())
+
+			By("NTP monitoring should be resolved")
+			Eventually(func(g Gomega) string {
+				g.Expect(k8sClient.Get(ctx, typeNamespaceName, found)).Should(Succeed())
+				return meta.FindStatusCondition(found.Status.Conditions, constants.Ready).Message
+			}).Should(Equal("NTP monitoring configured"))
+
+			By("Timestamp Authority deployment is updated")
+			Eventually(func(g Gomega) bool {
+				updated := &appsv1.Deployment{}
+				g.Expect(k8sClient.Get(ctx, types.NamespacedName{Name: actions.DeploymentName, Namespace: Namespace}, updated)).To(Succeed())
+				return equality.Semantic.DeepDerivative(deployment.Spec.Template.Spec.Volumes, updated.Spec.Template.Spec.Volumes)
+			}).Should(BeFalse())
 
 		})
 	})